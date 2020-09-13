3d621a904dae86efac788c3afa39d27167e6c389
Yonatan1P <yonimotek@gmail.com>
2020-09-13 00:59:57 -0700

    page layout initial setup

---
+++ apps.js
@@ -0,0 +1,16 @@
+'use strictly'
+
+function physicist(picture,firstName,lastName) = {
+    picture: 'link to picture of physicist',
+    firstName: 'first name of physicist',
+    lastName: 'last name of physicist'
+}
+
+var arrayofPhysicists = new Array[];
+var cardState;
+var currentQuestion=0;
+
+function beginGame(){
+    cardState=0;
+
+}
